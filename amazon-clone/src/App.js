bc6bfb200bce1458dd5bdf80b20f80c469a8516f
AdamMaley <adamemaley@yahoo.com>
2020-08-26 13:58:36 -0400

    header done

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -10,9 +10,16 @@ function App() {
     <Router>
       <div className="app">
         <Switch>
-          <Route path="/"><Header /><h1>Home Page</h1></Route>
-          <Route path="/checkout"><h1>Checkout Page</h1></Route>
-          <Route path="/login"><h1>Login Page</h1></Route>
+          <Route path="/checkout">
+            <h1>Checkout Page</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login Page</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <h1>Home Page</h1>
+          </Route>
         </Switch>
       </div>
     </Router>
